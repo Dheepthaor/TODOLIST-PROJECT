033a06e59b86b1cb4ec699d2bbc4f8cf26483778
DheepthaOR <dheeptha.o.r44@gmail.com>
2025-11-10 22:00:32 +0530

    Initial clean commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  const isLoggedIn = true;
+
+  if (isLoggedIn) {
+    return <h1>Welcome Back!</h1>;
+  } else {
+    return <h1>Please Log In</h1>;
+  }
+}
+
+export default App ;
+
---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import ToDolist from './ToDoList'
+
+
+
+
+
+
+
+
+
+
+createRoot(document.getElementById('root')).render(
+<ToDolist/>
+  
+)
+
